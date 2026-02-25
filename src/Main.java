0da0258ab71dcadbf8b27b20c611e830073de933
damiaoregis <damiao.v.serrana@gmail.com>
2026-02-25 11:35:13 -0300

    implementando um predicate com lanbda

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
+import entities.Product;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+    List<Product> list = new ArrayList<>();
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    list.add(new Product("TV",900.00));
+    list.add(new Product("Smatphone",1500.00));
+    list.add(new Product("Tablet",1200.00));
+    list.add(new Product("Notebook", 3000.00));
+
+    list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+    for (Product p : list) {
+       IO.println(p);
+    }
 }

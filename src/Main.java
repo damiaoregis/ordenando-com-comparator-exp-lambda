4869213c0c1a29fbb23f1362fb8a824c654c89eb
damiaoregis <damiao.v.serrana@gmail.com>
2026-02-25 12:26:00 -0300

    usando funçao map  com lambda

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.stream.Collector;
+
 import entities.Product;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,13 +8,13 @@ void main() {
     List<Product> list = new ArrayList<>();
 
     list.add(new Product("TV",900.00));
-    list.add(new Product("Smatphone",1500.00));
+    list.add(new Product("Smartphone",1500.00));
     list.add(new Product("Tablet",1200.00));
     list.add(new Product("Notebook", 3000.00));
     list.add(new Product("Mouse", 50.00));
     list.add(new Product("Monitor", 500.00));
     list.add(new Product("Headset", 76.00));
     
-    list.forEach(p -> p.setPrice(p.getPrice() * 1.1)); // Apply a 10% increase to all products
-    list.forEach(System.out::println); // Print all products
+    List<String> upperCaseNames = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+    upperCaseNames.forEach(System.out::println); 
 }
